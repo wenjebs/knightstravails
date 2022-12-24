88a4af217c3abdbee909829947745e5d964d7709
wenjie <wenjieb2@gmail.com>
2022-12-24 19:32:26 +0800

    Working knights travails

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # knightstravails
+
+
+Knights travails project for javascript.
+
+Algorithm to find the shortest path for a chess knight from 0,0 to a certain point
+printing out the points along the way.
--- script.js
+++ script.js
@@ -19,28 +19,64 @@ function knightMoves([initialX, initialY], [finalX, finalY]) {
         [-1,2]
     ]
     let queue = []
-    while (knight.X !== finalX && knight.Y !== finalY) {
+    let seen = new Set();
+    let paths = []
+    let count = 0;
+    seen.add([initialX, initialY])
+    while (knight.X !== finalX || knight.Y !== finalY) {
+        count += 1;
+        // console.log(knight.X, knight.Y)
         for (let i=0;i<moves.length;i++){
             let posX = knight.X+moves[i][0]
             let posY = knight.Y+moves[i][1]
-            if (posX > 0 && posY > 0 && posX < 7 && posY < 7) {
+            let containsMove = false
+            for (let arr of seen) {
+                if (arr.toString() === [posX, posY].toString()) {
+                    containsMove = true;
+                    break;
+                }
+            }
+            if (posX >= 0 && posY >= 0 && posX <= 7 && posY <= 7 && !containsMove) {
+                let path = {}
+                path.origin = [knight.X, knight.Y];
+                path.next = [posX, posY]
+                paths.push(path)
+                seen.add([posX, posY])
                 queue.push([posX, posY])
             }
-        }
-        queue.forEach((move) => {
-            let posX = move[0]
-            let posY = move[1]
-            if (posX > 0 && posY > 0 && posX < 7 && posY < 7) {
-                queue.push(move)
-            }
-        })
+        } 
+
         currentMove = queue.shift();
         knight.X = currentMove[0]
         knight.Y = currentMove[1]
-        console.log(currentMove)
+
     }
-    // for (let i=0;i<moves.length;i++){
-    //     queue.push([initialX+moves[i][0],initialY+moves[i][1]])
-    // }
-    // console.log(queue)
+
+    let finalPath;
+    let answer = [];
+    for (let path of paths) {
+        if (path.next.toString()===`${finalX},${finalY}`) {
+            finalPath = path
+        }
+    }
+    answer.push([finalPath.next])
+    while (finalPath.origin.toString() !== `0,0`) {
+        for (let path of paths) {
+            if (path.next.toString()===finalPath.origin.toString()) {
+                finalPath.next = path.next
+                finalPath.origin = path.origin
+                // console.log(finalPath)
+                break;
+            }
+        }
+        answer.push([finalPath.next])
+    }
+    console.log(paths)
+    answer.push([finalPath.origin])
+    answer.reverse()
+    console.log(`you took ${answer.length} steps!`)
+    console.log('The path was:')
+    for (let point of answer) {
+        console.log(point.toString())
+    }
 }
